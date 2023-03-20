226cbe0c57bf7076bf7255f9740bbd6be16ec516
bocharnikov-andrey <andrei.bocharnikov@bettorfaster.com>
2023-03-20 15:04:47 +0200

    Изменил логотип

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ContentPage from "./pages/ContentPage";
-import AdminPanel from "./pages/AdminPanel";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<ContentPage />} />
-        <Route path="/admin" exact={true} element={<AdminPanel />} />
-      </Routes>
-    </BrowserRouter>
+    <ContentPage />
   );
 }
 
